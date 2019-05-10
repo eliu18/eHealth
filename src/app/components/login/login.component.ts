074e5fbf2b78a3ecf9eb988a8f381533b6865d79
eliu18 <eliu.benitez18@gmail.com>
2019-05-10 18:07:32 -0500

    Change to reactive forms Login.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Login } from '../../interfaces/login';
 import { LoginService } from 'src/app/services/login.service';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,18 +9,26 @@ import { LoginService } from 'src/app/services/login.service';
 })
 
 export class LoginComponent implements OnInit {
-  loginForm: Login = {
-    user: '',
-    password: ''
-  };
-  constructor(private lS: LoginService) { }
+
+  constructor(private lS: LoginService, private fb: FormBuilder) { }
+
+  loginForm = this.fb.group({
+    user: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]]
+  });
 
   ngOnInit() {
   }
 
-  login() {
+  onSubmit() {
+    console.log('Form Submitted: ', this.loginForm);
     this.lS.login(this.loginForm).subscribe(data => {
       console.log(data);
+      console.log('Form Submitted: ', this.loginForm.value);
+      this.loginForm.patchValue({
+        user: '',
+        password: ''
+      });
     });
   }
 }
