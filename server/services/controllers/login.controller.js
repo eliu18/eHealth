7abc5af718a2c04ac15ef3373bff048b8bfd73a4
eliu18 <eliu.benitez18@gmail.com>
2019-05-10 18:08:38 -0500

    Create login controller at server.

---
+++ server/services/controllers/login.controller.js
@@ -0,0 +1,17 @@
+const DB = require('../../libs/mongoDb');
+
+class LoginController {
+    constructor() {}
+
+    getAllUsers(collection) {
+        const db = new DB();
+        return new Promise((resolve) => {
+            db.get(collection).then((users) => {
+                console.log(users);
+                resolve(users);
+            });
+        });
+    }
+}
+
+module.exports = LoginController;
