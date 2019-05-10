bd5bcbca7db1b5633a9e4743aea2141f240e7073
eliu18 <eliu.benitez18@gmail.com>
2019-05-10 18:10:25 -0500

    Update login routes.

--- server/services/apis/login.js
+++ server/services/apis/login.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const LC = require('../controllers/login.controller');
+const loginController = new LC();
+
+let collection = 'users';
+
 router.get('/', (req, res) => {
-    res.status(200).json({ message: "Ok" });
+    loginController.getAllUsers(collection).then((users) => {
+        res.status(200).json({ message: "Ok", users: users || 'Option2' });
+    });
 });
 
 module.exports = router;
