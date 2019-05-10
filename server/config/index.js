292ec9b45bf9f9961d0b77a3c7fe4140adac47b5
eliu18 <eliu.benitez18@gmail.com>
2019-05-10 18:08:11 -0500

    Update config at server.

--- server/config/index.js
+++ server/config/index.js
@@ -5,14 +5,22 @@ let config = {};
 if (process.env.NODE_ENV === "development") {
     config = {
         PORT: process.env.DEV_PORT,
-        HOST: process.env.DEV_HOST
-    }
+        HOST: process.env.DEV_HOST,
+        DB_USER: process.env.DEV_DB_USER,
+        DB_PASSWORD: process.env.DEV_DB_PASSWORD,
+        DB_HOST: process.env.DEV_DB_HOST,
+        DB_NAME: process.env.DEV_DB_NAME,
+    };
 
 } else if (process.env === "production") {
     config = {
-        PORT: process.env.PROD_PORT,
-        HOST: process.env.PROD_HOST
-    }
+        PORT: process.env.DEV_PORT,
+        HOST: process.env.DEV_HOST,
+        DB_USER: process.env.DEV_DB_USER,
+        DB_PASSWORD: process.env.DEV_DB_PASSWORD,
+        DB_HOST: process.env.DEV_DB_HOST,
+        DB_NAME: process.env.DEV_DB_NAME,
+    };
 }
 
 module.exports = config;
